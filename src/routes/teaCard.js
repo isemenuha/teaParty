8f5de97d69fa61816adc37f60ee2d931128122c6
alstvt <maxeebb20@gmail.com>
2023-10-12 09:05:37 +0300

    minor fixes

--- src/routes/teaCard.js
+++ src/routes/teaCard.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { Tea } = require('../../db/models'); 
+const { Comment } = require('../../db/models'); 
+const { User } = require('../../db/models'); 
 const renderTemplate = require('../lib/renderTemplate');
 const TeaCard = require('../views/TeaCard')
 
@@ -8,11 +10,11 @@ const TeaCard = require('../views/TeaCard')
 router.get('/tea-details', async (req, res) => {
 try {
     const tea = await Tea.findAll({ where: { id: req.query.id } })
-    console.log(tea)
+    const comments = await Comment.findAll({ where: {teaId: req.query.id} })
     if (!tea) {
     return res.status(404).json({ error: 'Чай не найден' });
     }
-    renderTemplate(TeaCard, { tea }, res)
+    renderTemplate(TeaCard, { tea, comments }, res)
 } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Внутренняя ошибка сервера' });
