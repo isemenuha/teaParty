2c07f09b87a90748f3082759dc2f8c6b1c734357
Иван Семенуха <60061625+isemenuha@users.noreply.github.com>
2023-10-12 09:25:40 +0300

    комменты, верстка

--- src/routes/teaCard.js
+++ src/routes/teaCard.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const { Tea } = require('../../db/models'); 
+const { Tea, Comment, User } = require('../../db/models');
 const renderTemplate = require('../lib/renderTemplate');
-const TeaCard = require('../views/TeaCard')
-
+const TeaCard = require('../views/TeaCard');
 
 router.get('/tea-details', async (req, res) => {
-try {
-    const tea = await Tea.findAll({ where: { id: req.query.id } })
-    console.log(tea)
+  try {
+    const { name } = req.session;
+
+    const tea = await Tea.findAll({ where: { id: req.query.id } });
+    const comments = await Comment.findAll({
+      where: { teaId: req.query.id },
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+    console.log(comments);
     if (!tea) {
-    return res.status(404).json({ error: 'Чай не найден' });
+      return res.status(404).json({ error: 'Чай не найден' });
     }
-    renderTemplate(TeaCard, { tea }, res)
-} catch (error) {
+    renderTemplate(TeaCard, { tea, comments, name }, res);
+  } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Внутренняя ошибка сервера' });
-}
+  }
 });
 
 module.exports = router;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,12 +2,13 @@ const React = require('react');
 
 const Layout = require('./Layout');
 
-
 module.exports = function Home({ name }) {
-  return <Layout name = { name }>
-     <script src="https://api-maps.yandex.ru/2.1/?apikey=25c96404-4795-41f1-a616-103183a0d71e&lang=ru_RU" type="text/javascript" />
-            <script src='/js/maps.js' />
-            <link rel='stylesheet' href='/css/maps.css'/>
-    <div id='map' className='map'>Карта</div>
-  </Layout>;
+  return (
+    <Layout name={name}>
+      <script src="https://api-maps.yandex.ru/2.1/?apikey=25c96404-4795-41f1-a616-103183a0d71e&lang=ru_RU" type="text/javascript" />
+      <script src="/js/maps.js" />
+      <link rel="stylesheet" href="/css/maps.css" />
+      <div id="map" className="map"></div>
+    </Layout>
+  );
 };
